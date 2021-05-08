778486436bbb7de8c01feb8dff2825257aeacde1
Rahul Raj <themailtorahul@gmail.com>
2021-05-08 21:15:20 +0530

    Completed the Code!

--- src/App.java
+++ src/App.java
@@ -22,16 +22,16 @@ public class App {
                     empCount++;
                     break;
                 case 2:
-                    // delete employee
+                    deleteEmployee();
                     break;
                 case 3:
                     displayEmployee();
                     break;
                 case 4:
-                    // Display all employees
+                    displayAllEmployees();
                     break;
                 case 5:
-                    // Check incentive eligibility
+                    checkIncentiveEligibility();
                     break;
                 case 6:
                     iterationFlag = false;
@@ -61,13 +61,13 @@ public class App {
 
     }
 
-    public static void displayEmployee(){
+    public static void displayEmployee() {
         System.out.print("Enter the employee ID : ");
         int id = sc.nextInt();
         boolean found = false;
 
         for (int i = 0; i < empCount; i++) {
-            if(employee[i].getEmpId() == id){
+            if (employee[i].getEmpId() == id) {
                 System.out.println("Name : " + employee[i].getEmpName());
                 System.out.println("ID : " + employee[i].getEmpId());
                 System.out.println("Position : " + employee[i].getPosition());
@@ -78,6 +78,69 @@ public class App {
                 break;
             }
         }
-        if(!found) System.out.println("Employee not found!");
+        if (!found)
+            System.out.println("Employee not found!");
+    }
+
+    public static void deleteEmployee() {
+        System.out.print("Enter the employee ID : ");
+        int id = sc.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < empCount; i++) {
+            if (employee[i].getEmpId() == id) {
+                for (int j = i; j < empCount; j++) {
+                    employee[j] = employee[j + 1];
+                }
+                empCount--;
+                found = true;
+                System.out.println("Employee deleted Successfully!");
+                break;
+            }
+        }
+        if (!found)
+            System.out.println("Employee not found!");
+    }
+
+    public static void displayAllEmployees() {
+        boolean found = false;
+        for (int i = 0; i < empCount; i++) {
+            System.out.println("\n\n\nName : " + employee[i].getEmpName());
+            System.out.println("ID : " + employee[i].getEmpId());
+            System.out.println("Position : " + employee[i].getPosition());
+            System.out.println("Grade : " + employee[i].getGrade());
+            System.out.println("Experience : " + employee[i].getExperience());
+            System.out.println("Appraisal Score : " + employee[i].getAppraisalScore());
+            found = true;
+        }
+        if (!found)
+            System.out.println("No Employees to display!");
+    }
+
+    public static void checkIncentiveEligibility() {
+        System.out.print("Enter the employee ID : ");
+        int id = sc.nextInt();
+        boolean eligibility = false;
+        String candidate = "";
+
+        for (int i = 0; i < empCount; i++) {
+            if (employee[i].getEmpId() == id) {
+                if (employee[i].getExperience() >= 5)
+                    eligibility = true;
+                else if (employee[i].getExperience() < 5 && employee[i].getExperience() >= 3
+                        && employee[i].getAppraisalScore() >= 70)
+                    eligibility = true;
+                else if (employee[i].getExperience() < 3 && employee[i].getAppraisalScore() >= 90)
+                    eligibility = true;
+
+                candidate = employee[i].getEmpName();
+                break;
+            }
+        }
+
+        if (eligibility)
+            System.out.println(candidate + " is eligible for Performance Incentive.");
+        else
+            System.out.println(candidate + " is Not Eligible for performance Incentive.");
     }
 }
