1449298ef491293d9b1e2408a4fc6a2dd2924294
Rahul Raj <themailtorahul@gmail.com>
2021-05-08 13:10:00 +0530

    Added addEmployee and displayEmployee

--- src/App.java
+++ src/App.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
+import Enterprise.*;
+
 public class App {
+    static Employee[] employee = new Employee[10];
+    static int empId = 0;
+    static int empCount = 0;
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+
         boolean iterationFlag = true;
 
         do {
@@ -11,13 +18,14 @@ public class App {
 
             switch (choice) {
                 case 1:
-                    // add employee
+                    employee[empCount] = addEmployee();
+                    empCount++;
                     break;
                 case 2:
                     // delete employee
                     break;
                 case 3:
-                    // Display employee
+                    // displayEmployee();
                     break;
                 case 4:
                     // Display all employees
@@ -33,7 +41,43 @@ public class App {
             }
         } while (iterationFlag);
         System.out.println("\nThank you. Come again!");
+    }
 
-        
+    public static Employee addEmployee() {
+        empId++;
+        System.out.print("Enter the name : ");
+        sc.nextLine();
+        String name = sc.nextLine();
+
+        System.out.print("Enter the Appraisal Score : ");
+        double aScore = sc.nextDouble();
+        System.out.print("Enter experience in years : ");
+        int exp = sc.nextInt();
+        System.out.print("Enter the grade : ");
+        int grade = sc.nextInt();
+
+        Employee temp = new Employee(name, empId, aScore, exp, grade);
+        return temp;
+
+    }
+
+    public static void displayEmployee(){
+        System.out.print("Enter the employee ID : ");
+        int id = sc.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < empCount; i++) {
+            if(employee[i].getEmpId() == id){
+                System.out.println("Name : " + employee[i].getEmpName());
+                System.out.println("ID : " + employee[i].getEmpId());
+                System.out.println("Position : " + employee[i].getPosition());
+                System.out.println("Grade : " + employee[i].getGrade());
+                System.out.println("Experience : " + employee[i].getExperience());
+                System.out.println("Appraisal Score : " + employee[i].getAppraisalScore());
+                found = true;
+                break;
+            }
+        }
+        if(!found) System.out.println("Employee not found!");
     }
 }
